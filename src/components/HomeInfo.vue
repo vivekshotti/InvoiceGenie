3c1962a03ae44a5af174546df40259602e0d50b4
Vivek Hotti <vivekshotti@gmail.com>
2022-06-14 20:36:07 +0530

    Initial Commit 14.6.22

---
+++ src/components/HomeInfo.vue
@@ -0,0 +1,24 @@
+<script>
+  export default {
+      name: 'HomeInfo',
+  } 
+</script>
+
+<template>
+
+    <div class="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+      Your One Stop <p class="underline underline-offset-8 decoration-indigo-500">Invoicing Solution.</p><br/>
+      🤯<br/>
+    </div>
+    <p class="leading-8 mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+      InvoiceGenie lets you create invoices in a flick of a wind.<br/>
+      It helps you monitor all your invoices, update their statuses, and track it efficiently.<br/>
+      <br/>
+      YOUR responsibilities are now OURS. 
+    </p>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,38 @@
+<script>
+  import HomeInfo from "../components/HomeInfo.vue";
+
+  export default {
+  name: 'App',
+  components: {
+    HomeInfo
+},
+  data() {
+    return {
+
+    }
+  },
+  methods:
+  {
+
+  },
+  
+}
+
+</script>
+
+<template>
+  <div>
+    <div class="h-screen m-8 flex flex-row space-x-4">
+      <div class="p-10 border-solid border-2 border-sky-500 basis-1/2">
+        <HomeInfo/>
+      </div>
+      <div class="p-10 border-solid border-2 border-sky-500 basis-1/2">
+        yopity yopity yo
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  
+</style>
