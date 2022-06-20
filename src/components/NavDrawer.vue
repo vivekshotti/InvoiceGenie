918f4bb085c119b875f88c8759126e3cfe527b56
Vivek Hotti <vivekshotti@gmail.com>
2022-06-20 18:24:48 +0530

    Fifth Commit - NavDrawer

---
+++ src/components/NavDrawer.vue
@@ -0,0 +1,68 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="container space-y-3 divide-y divide-slate-200">
+    <div class="text-3xl font-extrabold dark:text-black">InvoiceGenie</div>
+
+    <div class="grid grid-rows-6 grid-flow-col gap-1">
+
+        <div>
+
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg class="h-8 w-8 text-black"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <circle cx="12" cy="13" r="2" />  <line x1="13.45" y1="11.55" x2="15.5" y2="9.5" />  <path d="M6.4 20a9 9 0 1 1 11.2 0Z" /></svg>
+               <span class="ml-3">Dashboard</span>
+            </a>
+
+        </div>
+        <div>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <svg class="h-8 w-8 text-black"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <polyline points="12 4 4 8 12 12 20 8 12 4" />  <polyline points="4 12 12 16 20 12" />  <polyline points="4 16 12 20 20 16" /></svg>
+                <span class="flex-1 ml-3 whitespace-nowrap">Invoices</span>
+            </a>
+        </div>
+        <div>
+            
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg class="h-8 w-8 text-black"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <line x1="9" y1="12" x2="15" y2="12" />  <line x1="12" y1="9" x2="12" y2="15" />  <path d="M4 6v-1a1 1 0 0 1 1 -1h1m5 0h2m5 0h1a1 1 0 0 1 1 1v1m0 5v2m0 5v1a1 1 0 0 1 -1 1h-1m-5 0h-2m-5 0h-1a1 1 0 0 1 -1 -1v-1m0 -5v-2m0 -5" /></svg>
+               <span class="ml-3">New Invoice</span>
+            </a>
+        </div>
+        <div>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg class="h-8 w-8 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"/></svg>
+               <span class="ml-3">Profile</span>
+            </a>
+        </div>
+        <div>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+               <svg class="h-8 w-8 text-black"  fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7v8a2 2 0 002 2h6M8 7V5a2 2 0 012-2h4.586a1 1 0 01.707.293l4.414 4.414a1 1 0 01.293.707V15a2 2 0 01-2 2h-2M8 7H6a2 2 0 00-2 2v10a2 2 0 002 2h8a2 2 0 002-2v-2"/></svg>
+
+               <span class="ml-3">Documentation</span>
+            </a>
+        </div>
+        <div>
+            <a href="#" class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
+                <svg class="h-8 w-8 text-black"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">  <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2" /></svg>
+               <span class="ml-3">Go Premium</span>
+            </a>
+        </div>
+
+
+
+    </div>
+
+
+</div>
+
+</template>
+
+<style scoped>
+.center{
+
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,4 +1,41 @@
+<script>
+import NavDrawer from "../components/NavDrawer.vue";
+
+  export default {
+  name: 'DashboardView',
+  components: {
+    NavDrawer,
+
+},
+  data() {
+    return {
+
+    }
+  },
+  methods:
+  {
+
+  }
+  }
+
+</script>
+
 <template>
-<div>Yolo</div>
+<div class="grid-container grid grid-cols-6 divide-x">
+  <div class="h-screen item1 col-span-1 shadow-md">
+    <div class="m-5">
+        <NavDrawer/>
+    </div>
+  </div>
+  <div class="item2 col-span-5 bg-gray-100">
+    <!-- <NavDrawer/> -->
+  </div>
+</div>
 
 </template>
+
+<style scoped>
+
+
+
+</style>
