fe8412cce7e4d8909decc6f176e9c464a9ecda01
Vivek Hotti <vivekshotti@gmail.com>
2022-06-23 17:17:13 +0530

    Architecture change

---
+++ server/src/app.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')))
+
+module.exports = app;
